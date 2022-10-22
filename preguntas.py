5b383c7b06a70e178356593bfe92543b1cd8202e
mblandonp <101910407+mblandonp@users.noreply.github.com>
2022-10-22 16:12:12 -0500

    commit

--- preguntas.py
+++ preguntas.py
@@ -46,7 +46,7 @@ def pregunta_02():
 
     # Defina el parámetro inicial `params` como un arreglo de tamaño 3 con ceros
     intercept_ = np.mean(np.array(y))
-    params = np.mean(np.array[3].shape)
+    params = np.mean(np.array(3))
     for i in range(n_iterations):
 
         # Compute el pronóstico con los parámetros actuales
